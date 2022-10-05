2011c06b133782f621843f8ca100679fec45e317
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-05 01:14:33 -0300

    Settings up the User Interface

---
+++ CameraFilter/CameraFilter/PhotoCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewController.swift
+//  CameraFilter
+//
+//  Created by Henrique Alves Batochi on 05/10/22.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewController: UICollectionViewController {
+    
+    
+    
+}
---
+++ CameraFilter/CameraFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Henrique Alves Batochi on 05/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
+
