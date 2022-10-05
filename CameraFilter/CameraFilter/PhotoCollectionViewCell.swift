715b2e6d3d652836737dc98bbcbda1edd1ea7f76
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-05 11:01:43 -0300

    Displaying photos from photo library

---
+++ CameraFilter/CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by Henrique Alves Batochi on 05/10/22.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
